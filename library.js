128f28ff2a07af2706451be8d39c029d908bdc2f
Ezrovat <danic97@gmail.com>
2025-12-26 13:57:48 +0100

    add modal backbone

--- library.js
+++ library.js
@@ -1,13 +1,22 @@
 const books = [];
 
-function Book(title, author, price, genre) {
+function Book(title, author, price, genre, imgLink, imgFile) {
     this.id = crypto.randomUUID();
     this.title = title ?? null;
     this.author = author ?? null;
     this.price = Number.isNaN(+price) ? null : +price;
     this.genre = genre ?? null;
+    this.imgLink = imgLink ?? null;
+    this.imgFile = imgFile;
 }
 
-function addBookToLibrary(title, author, price, genre) {
-    books.push(new Book(title, author, price, genre));
+function addBookToLibrary(title, author, price, genre,imgLink,imgFile) {
+    books.push(new Book(title, author, price, genre,imgLink, imgFile));
 }
+
+const dialog = document.querySelector("dialog");
+const newBookButton = document.querySelector(".new-book > button");
+const cancelButton = document.querySelector("#cancel");
+
+newBookButton.addEventListener("click", () => dialog.showModal());
+cancelButton.addEventListener("click", () => dialog.close());
